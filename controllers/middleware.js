ef9b09448cae134ef9be461073a2c4fe06c99a42
Arcie Aquino <aquinoarcie@gmail.com>
2024-08-15 20:09:14 +0800

    formdata test fix. put isLoggedIn middleware back to subd /create

--- controllers/middleware.js
+++ controllers/middleware.js
@@ -6,11 +6,14 @@ const { ACCESS_TOKEN_SECRET } = process.env;
 
 const isLoggedIn = async (req, res, next) => {
 	try {
+		console.log("isLoggedIn", req.body);
 		if (req.headers.authorization) {
+			console.log("isLoggedIn", req.body);
 			const token = req.headers.authorization.split(" ")[1];
 			if (token) {
 				const payload = jwt.verify(token, ACCESS_TOKEN_SECRET);
 				if (payload) {
+					console.log("isLoggedIn", req.body);
 					req.user = payload;
 					next();
 				} else {
